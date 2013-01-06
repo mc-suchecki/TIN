4aac560006d2aee5d55f8c4995d18731fecbe0b2
Przemek <barcikowski.przemyslaw@gmail.com>
2013-01-06 14:28:45 +0100

    Update of older files: attachment to previous push

--- server/Server.cpp
+++ server/Server.cpp
@@ -1,14 +1,36 @@
-#include "Server.h"
+/*
+ * Server.cpp
+ *
+ *  Created on: 26-12-2012
+ *      Author: przemek
+ */
+
+#include "Server.hpp"
 
 
 Server::Server(int port)
 {
-    connectionHandler = new ConnectionHandler(port);
+	commandQueue = new BlockingQueue<std::string/*Command*/>;
+	resultFileQueue = new BlockingQueue<std::string/*Command*/>;
+
+	connectionHandler = new ConnectionHandler(port,
+											  commandQueue,
+											  resultFileQueue);
     connectionHandler->start();
+
+    commandExecutor = new CommandExecutor(commandQueue,
+    									  resultFileQueue);
+    commandExecutor->start();
 }
 
 Server::~Server()
 {
     connectionHandler->join();
     delete connectionHandler;
+
+    commandExecutor->join();
+	delete commandExecutor;
+
+	delete commandQueue;
+	delete resultFileQueue;
 }
--- server/main.cpp
+++ server/main.cpp
@@ -1,6 +1,13 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 26-12-2012
+ *      Author: przemek
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
-#include "Server.h"
+#include "Server.hpp"
 
 using namespace std;
 
@@ -15,3 +22,8 @@ int main(int argc, char *argv[])
      Server server = Server(atoi(argv[1]));
      return 0;
 }
+
+
+//TODO
+//- binary messages
+//- logger?
